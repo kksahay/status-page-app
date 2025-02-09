c92d5847d681222fc87fd212127ae2ea50e3a07c
kksahay <kunal.sahay4@gmail.com>
2025-02-09 09:26:09 +0530

    socket uri

--- client/src/api/http.ts
+++ client/src/api/http.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const getToken = () => localStorage.getItem("token");
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // Update with your actual API URL
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
